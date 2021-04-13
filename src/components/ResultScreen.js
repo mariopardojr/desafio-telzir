1c5d2406b14dc5b157178dab4127615f9159f3be
Mario Pardo <mariio.pardojr@gmail.com>
2021-04-13 13:49:55 -0300

    Adiciona animattion na tela de resultado

--- src/components/ResultScreen.js
+++ src/components/ResultScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropsTypes from 'prop-types';
 import style from '../styles/Components/ResultScreen.module.css';
+import animation from '../styles/Animations.module.css';
 
 class ResultScreen extends React.Component {
   render() {
     const { data: { service, result }, handleClick } = this.props;
     const { withPlan, withoutPlan } = result;
     return (
-      <div className={ style.container }>
+      <div className={ `${style.container} ${animation.fadeInUp}` }>
         <div className={ style.result }>
           <h3>{`Com ${service}`}</h3>
           <p>{`Essa ligação custará $${parseFloat(withPlan.total).toFixed(2)}`}</p>
